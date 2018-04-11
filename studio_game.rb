942b0949a57e1a931d13ccf69ba1d9d668dd946f
Luis Amez <luis.amez.vitoria@gmail.com>
2018-04-11 13:23:41 +0200

    Add getters and setters to Player

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,14 @@
-class Player 
+class Player
+  attr_accessor :name
+  attr_reader :health
+
   def initialize(name, health = 100)
     @name = name.capitalize
     @health = health
   end
 
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def blam
@@ -17,12 +20,26 @@ class Player
     @health += 15
     puts "#{@name} got w00ted!"
   end
+
+  def score
+    @name.length + @health
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
 end
 
 player1 = Player.new("moe")
 puts player1
 
 player2 = Player.new("larry", 60)
+puts player2
+puts player2.name
+player2.name = "lawrence"
+puts player2.name
+puts player2.health
+puts player2.score
 puts player2
 
 player3 = Player.new("curly", 125)
