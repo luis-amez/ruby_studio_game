bc1ea7e63706f6515210c2c28ff5a2eafbae59e7
Luis Amez <luis.amez.vitoria@gmail.com>
2018-04-12 16:52:17 +0200

    Add prompt for user to choose number of rounds

--- studio_game.rb
+++ studio_game.rb
@@ -9,7 +9,19 @@ knuckleheads = Game.new("Knuckleheads")
 knuckleheads.add_player(player1)
 knuckleheads.add_player(player2)
 knuckleheads.add_player(player3)
-knuckleheads.play(10) do
-  knuckleheads.total_points >= 2000
+loop do
+  puts "How many game rounds? ('quit' to exit)"
+  answer = gets.chomp.downcase
+  case answer
+  when /^\d+$/
+    knuckleheads.play(answer.to_i) do
+      knuckleheads.total_points >= 2000
+    end
+  when 'quit', 'exit'
+    knuckleheads.print_stats
+    break
+  else
+    puts "Please enter a number or 'quit'"
+  end
 end
-knuckleheads.print_stats
+
